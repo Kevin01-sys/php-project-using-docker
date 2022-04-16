78be07c07e55ade84740dbe7b22d96fe80bc415e
Kevin Chocano <kevinchocano@gmail.com>
2022-04-15 23:01:14 -0400

    modified: index.html english  comments

--- www/sql files/db.sql
+++ www/sql files/db.sql
@@ -1,6 +1,48 @@
 create database tutorial;
 use tutorial;
-create table usuarios  (id int auto_increment,
-						nombre varchar(50),
-                        hobby varchar(50),
-						primary key(id));
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(25) COLLATE utf8_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (2,'hiza','$2y$10$duEq4sTWABEnRB4gnkqRNu37q7YCPOKo3xBocphYPD1Mbs.ruP5Rm','kevinchocano@gmail.com');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `run` varchar(50) NOT NULL,
+  `nombre` varchar(50) DEFAULT NULL,
+  `hobby` varchar(50) DEFAULT NULL,
+  `estado` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=62 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (14,'1452','Tanjiro','Cuidar a Nezuko',1),(15,'4566','Hiza','Programar',1),(16,'7890','Kanroji','Amar a iguro u//u',1),(19,'19111','Iguro','Amar a mituri uwu ',1),(20,'6543','Himejima','El pilar más grande',1),(61,'123456789','qwerty','qwerty',1);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
